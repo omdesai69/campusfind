f28cbcf2c642076ad8d1894334a04617343c387c
omiboi <omdesai608@gmail.com>
2026-07-01 12:28:32 +0530

    feat(formatting): add INR currency formatter with Indian numbering

---
+++ src/lib/currencyFormatter.ts
@@ -0,0 +1,3 @@
+export function formatIndianCurrency(amount: number): string {
+  return new Intl.NumberFormat('en-IN', { style: 'currency', currency: 'INR', maximumFractionDigits: 0 }).format(amount);
+}
